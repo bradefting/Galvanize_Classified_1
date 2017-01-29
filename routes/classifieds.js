7b756bd314a57f1dc4bc844961a8979f1e25c094
Brad Efting <bradefting@gmail.com>
2017-01-29 15:21:08 -0700

    patch working and passing 6 tests passing

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -48,8 +48,24 @@ router.post('/', (req, res, next) =>{
      });
 });
 
-
 //update
+router.patch('/:id', (req, res, next) =>{
+  knex('classifieds')
+     .where('id', req.params.id)
+     .update({
+       title: req.body.title,
+       description: req.body.description,
+       price: req.body.price,
+       item_image: req.body.item_image
+     }, ['id', 'title', 'description', 'price', 'item_image'])
+     .then((results)=>{
+       res.send(results[0]);
+     })
+     .catch((err)=>{
+       next(err);
+     });
+});
+
 
 //delete
 
