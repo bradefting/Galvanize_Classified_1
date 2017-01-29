d5f9f10957b9c02616b539f6bfe46c36d6a5efba
Brad Efting <bradefting@gmail.com>
2017-01-29 14:57:39 -0700

    get all and get by id routes work 4 tests passing

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -2,9 +2,40 @@
 'use strict';
 
 const express = require('express');
-
 const router = express.Router();
+const knex = require('../knex')
 
 // YOUR CODE HERE
+
+//get all
+router.get('/', (req, res, next) =>{
+  knex('classifieds')
+     .select('id', 'title', 'description', 'price', 'item_image')
+     .then((results)=>{
+       res.send(results);
+     })
+     .catch((err)=>{
+       next(err);
+     });
+});
+
+//get by id
+router.get('/:id', (req, res, next) =>{
+  knex('classifieds')
+     .select('id', 'title', 'description', 'price', 'item_image')
+     .where('id', req.params.id)
+     .then((results)=>{
+       res.send(results[0]);
+     })
+     .catch((err)=>{
+       next(err);
+     });
+});
+
+//post
+
+//update
+
+//delete
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,15 @@
 
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
 const messages = require('./routes/classifieds');
+const port = process.env.PORT || 3000;
 
 app.use('/classifieds',messages);
 
-const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.listen(port, () => {
   console.log('Listening on port', port);
