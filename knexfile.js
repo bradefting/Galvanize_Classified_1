e07486db8841c4a1090e26918ce700745a4d3d26
Brad Efting <bradefting@gmail.com>
2017-01-29 13:09:24 -0700

    create knexfile connections

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,19 @@
 // Update with your config settings.
+"use strict";
 
 module.exports = {
 
-  development: { },
-  test: { },
-  production: { },
+  development: {
+    client: 'pg',
+    conenction: 'postgress://localhost/classifieds_dev'
+  },
+  test: {
+    client: 'pg',
+    conenction: 'postgress://localhost/classifieds_test'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  },
 
 };
